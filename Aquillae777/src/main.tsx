45e5dd6e94072852c1a2fca5127a054978028d62
aquillae777 <aquillae777@gmail.com>
2023-03-22 02:06:04 -0300

    condig pacotes

--- Aquillae777/src/main.tsx
+++ Aquillae777/src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import Home from "../pags/Home"
+import {BrowserRouter , Routes, Route} from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="" element={<Home/>}/>
+      
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
