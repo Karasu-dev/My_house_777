c0fdeee8679a55da6049b73cf7dd1832679147cd
Aquillae777 <Aquillae777@gmail.com>
2023-04-18 18:26:08 -0300

    erro 404

--- Aquillae777/pags/Home.tsx
+++ Aquillae777/pags/Home.tsx
@@ -14,7 +14,7 @@ scroll-behavior: smooth;
 overflow-y: auto;
 scrollbar-width: none;
 color: white;
-background-image: url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.depositphotos.com%2Fstock-photos%2Funivers.html&psig=AOvVaw0yIyd2tvvlCDwK5WDSUNpF&ust=1681939374865000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCKjW48mutP4CFQAAAAAdAAAAABAE");
+background: url("Aquillae777/img/i349388.webp");
 z-index: -10;
 background: black
 &::-webkit-scrollbar{
