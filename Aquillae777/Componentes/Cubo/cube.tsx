d4de5c6e465f00407964c986f0055af4e7f26876
Aquillae777 <Aquillae777@gmail.com>
2023-04-21 23:53:31 -0300

    Finish

--- Aquillae777/Componentes/Cubo/cube.tsx
+++ Aquillae777/Componentes/Cubo/cube.tsx
@@ -18,7 +18,7 @@ function Box(props: ThreeElements['mesh']) {
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}>
       <boxGeometry args={[1,1,1]} />
-      <meshStandardMaterial color={hovered ? 'white' : 'black' } />
+      <meshStandardMaterial color={hovered ? '#4E1F1F' : '#303050' } />
     </mesh>
   )
 }
@@ -39,7 +39,7 @@ function Box2(props: ThreeElements['mesh']) {
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}>
       <boxGeometry args={[1,1,1]} />
-      <meshStandardMaterial color={hovered ? 'white' : 'black' } />
+      <meshStandardMaterial color={hovered ? '#4E1F1F' : '#303050' } />
     </mesh>
   )
 }
@@ -60,7 +60,7 @@ function Box3(props: ThreeElements['mesh']) {
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}>
       <boxGeometry args={[1,1,1]} />
-      <meshStandardMaterial color={hovered ? 'black' : 'black' } />
+      <meshStandardMaterial color={hovered ? '#4E1F1F' : '#303050' } />
     </mesh>
   )
 }
@@ -73,26 +73,33 @@ const Cubo = () => {
     return(  
          <Canvas>
         <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[1, 0, 0]} />
-
-        <pointLight position={[5, 5, 5]} />
-        <Box2 position={[1, 0, 0]} />
-
-
-        <pointLight position={[5, 5, 5]} />
-        <Box3 position={[1, 0, 0]} />
+       
 
 
         <pointLight position={[20, 20, 20]} />
-        <Box position={[1, 1, 0]} />
+        <Box position={[-1, 1, 0]} />
 
         <pointLight position={[30, 30, 30]} />
-        <Box2 position={[1, 1, 0]} />
+        <Box2 position={[-1, 1, 0]} />
 
 
         <pointLight position={[40, 50, 60]} />
-        <Box3 position={[1, 1, 0]} />
+        <Box3 position={[-1, 1, 0]} />
+
+
+        <pointLight position={[20, 20, 20]} />
+        <Box position={[1, 2, 0]} />
+
+        <pointLight position={[30, 30, 30]} />
+        <Box2 position={[1, 2, 0]} />
+
+
+        <pointLight position={[40, 50, 60]} />
+        <Box3 position={[1, 2, 0]} />
+
+     
+
+        
 
         
 
