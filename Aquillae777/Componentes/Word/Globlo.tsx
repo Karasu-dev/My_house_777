d5dee74d39e311402065c3fbea03db3695a268d6
Aquillae777 <Aquillae777@gmail.com>
2023-05-15 22:23:01 -0300

    Word

--- Aquillae777/Componentes/Word/Globlo.tsx
+++ Aquillae777/Componentes/Word/Globlo.tsx
@@ -11,33 +11,45 @@ import styled from "styled-components";
 import Img from "../../../Img/i349388.webp";
 
 const color = "yellow";
+const color3 = "";
 const color2 = "white"; 
 const Icosahedron = () => (
   <mesh rotation-x={10} rotation-y={10} >
     <icosahedronGeometry args={[1, 9,]} />
     <meshBasicMaterial wireframe color={color} />
+    
+  </mesh>
+);
+
+
+const Icosahedron2 = () => (
+  <mesh rotation-x={10} rotation-y={10} >
+    <icosahedronGeometry args={[50, 5,]} />
+    <meshBasicMaterial wireframe color={color3} />
+    
   </mesh>
 );
 const Star = ({ p }: { p: number }) => {
   const meshRef = useRef<Mesh>(null);
   useLayoutEffect(() => {
-    const distance = mix(2, 3.5, Math.random());
+    const distance = mix(10, 45, Math.random());
     const yAngle = mix(
-      degreesToRadians(80),
-      degreesToRadians(100),
+      degreesToRadians(10),
+      degreesToRadians(1000),
       Math.random()
-    );0
-    const xAngle = degreesToRadians(360) * p;
+    );500
+    const xAngle = degreesToRadians(1000) * p;
     meshRef.current!.position.setFromSphericalCoords(distance, yAngle, xAngle);
   });
   return (
     <mesh ref={meshRef}>
-      <boxGeometry args={[0.1, 0.05, 0.05]} />
+      <boxGeometry args={[0.10, 0.100, 0.10]} />
       <meshBasicMaterial wireframe color={color2}/>
+      
     </mesh>
   );
 };
-function Scene({ numStars = 30 }) {
+function Scene({ numStars = 500 }) {
   const gl = useThree((state) => state.gl);
   const { scrollYProgress } = useScroll();
   const yAngle = useTransform(
@@ -59,7 +71,7 @@ function Scene({ numStars = 30 }) {
   useLayoutEffect(() => gl.setPixelRatio(1));
   const stars = [];
   for (let i = 0; i < numStars; i++) {
-    stars.push(<Star p={progress(0, numStars, i)} />);
+    stars.push(<Star p={progress(36, numStars, i)} />);
   }
   return (
     <>
