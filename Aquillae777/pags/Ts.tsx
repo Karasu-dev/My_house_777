e7f61329fd12150673e3b1697f358a50596c3c3a
Aquillae777 <Aquillae777@gmail.com>
2023-05-15 22:31:01 -0300

    .

--- Aquillae777/pags/Ts.tsx
+++ Aquillae777/pags/Ts.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import NavBar from "../Componentes/navbar/Navbar";
 import styled from "styled-components";
-import Globlo2 from "../Componentes/Word/Globo2";
 
 const Container = styled.div `
 background: black;
