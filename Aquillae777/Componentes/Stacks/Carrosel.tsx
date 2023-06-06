7212a6487c12533e43929e92b31cbfa047f74854
Aquillae777 <Aquillae777@gmail.com>
2023-06-06 08:21:53 -0300

    icons

--- Aquillae777/Componentes/Stacks/Carrosel.tsx
+++ Aquillae777/Componentes/Stacks/Carrosel.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import styled, { Keyframes, keyframes } from "styled-components";
+import IMG from "../../../Img/Meu projeto.png";
+import IMGdois from "../../../Img/iconw/java.png";
+import IMGtres from "../../../Img/iconw/typescript.png";
+import IMGquatro from "../../../Img/iconw/sql-server.png";
+import IMGcinco from "../../../Img/iconw/dados-binarios.png";
+
+
 
 const Section = styled.div`
 height: 200vh;
@@ -32,8 +39,6 @@ justify-content: space-between;
   justify-content: center;
 }
 `;
-
-
 
 
 const Key = keyframes`
@@ -71,10 +76,8 @@ height: 10px;
 line-height: 5px;
 transition:  background 0.100s;
 transform: translate(-2%, 1100%);
-
 text-decoration: none;
 color: #fff;
-
   &:before, &:after {
       display: block;
       content: 'Projects';
@@ -102,45 +105,32 @@ color: #fff;
   @media only screen and (max-width: 780px) {
     transform: translate(-2%, 10%);
   }
-
 }`
 
 const Title = styled.h3`
 Font-size: 30px;
 height: 200px;
 width: 300px;
-
 `
 
-
-const List = styled.div`
-
-`
-const Img = styled.image`
-`
 
 const Ferramentas = styled.li`
 Font-size: 20px;
+pointer: none;
+height: 25px;
+widht: 1px;
 
 `
 
 const ContainerBox = styled.div`
 display: flex;
 justify-content: space-between;
-padding: 20px;
-
-
-
+padding: 10px;
 @media only screen and (max-width: 780px) {
   display: inline;
   height: auto;
   display: block;
-   
-  
-}
-
-
-`
+}`
 
 const Box1 = styled.div`
 padding: 5px;
@@ -152,17 +142,12 @@ background: rgba(255, 0, 0, 0.14);
 border-width: 5px;
 border-radius: 20px;
 box-shadow: rgba(217, 24, 22, 0.4) -5px 10px, rgba(217, 24, 22, 0.3) -15px 20px, rgba(217, 24, 22, 0.2) -20px 25px, rgba(217, 24, 22, 0.1) -25px 30px, rgba(217, 24, 22, 0.0) -30px 35px;
-
-
-
-
 animation: box1 10s infinite ease alternate ;
 @keyframes box1 {
   to {
     transform: translateY(200px);
   }
 }
-
 @media only screen and (max-width: 780px) {
   height: auto;
   width: auto;
@@ -178,8 +163,6 @@ animation: box1 10s infinite ease alternate ;
   
 
 }
-
-
 `
 
 const Box2 = styled.div`
@@ -192,11 +175,6 @@ border-width: 5px;
 background: rgba(0, 0, 255, 0.14);
 border-radius: 20px;
 box-shadow: rgba(0, 38, 255, 0.4) -5px 10px, rgba(0, 38, 255,  0.3) -15px 20px, rgba(0, 38, 255,  0.2) -20px 25px, rgba(0, 38, 255,  0.1) -25px 30px, rgba(217, 24, 22, 0.0) -30px 35px;
-
-
-
-
-
 animation: box2 10s infinite ease alternate ;
 @keyframes box2 {
   to {
@@ -218,7 +196,6 @@ animation: box2 10s infinite ease alternate ;
 
 }
 `
-
 
 const Box3 = styled.div`
 padding: 5px;
@@ -248,7 +225,7 @@ animation: box3 10s infinite ease alternate ;
   height: auto;
   width: auto;
   padding: 10px;
-  margin: 60px;
+  margin: 50px;
   @keyframes box3 {
     to {
       transform: translateY(100px);
@@ -257,6 +234,7 @@ animation: box3 10s infinite ease alternate ;
 
 }
 `
+
 const Box4 = styled.div`
 color: green;
 padding: 5px;
@@ -281,7 +259,7 @@ animation: box4 4s infinite ease alternate ;
   height: auto;
   width: auto;
   padding: 10px;
-  margin: 80px;
+  margin: 50px;
   animation: box4 10s infinite ease alternate ;
   @keyframes box4 {
     to {
@@ -293,112 +271,12 @@ animation: box4 4s infinite ease alternate ;
 }
 `
 
-const Box5 = styled.div`
-padding: 5px;
+const Img = styled.img` 
+padding: 10px;
 margin: 10px;
-width:50%;
-height: 500px;
-border-style: solid;
-border-width: 5px;
-
-box-shadow: rgba(240, 46, 170, 0.4) -5px 10px, rgba(240, 46, 170, 0.3) -15px 20px, rgba(240, 46, 170, 0.2) -20px 25px, rgba(240, 46, 170, 0.1) -25px 30px, rgba(240, 46, 170, 0.05) -30px 35px;
-
-
-animation: box1 1s infinite ease alternate ;
-@keyframes box1 {
-  to {
-    transform: translateY(70px);
-  }
-}
-
-@media only screen and (max-width: 780px) {
-  height: auto;
-  width: auto;
-  padding: 10px;
-  
-
- 
-}
-
-
-`
-
-const Box6 = styled.div`
-padding: 5px;
-margin: 10px;
-width:50%;
-height: 550px;
-border-style: solid;
-border-width: 5px;
-box-shadow: rgba(240, 46, 170, 0.4) -5px 5px, rgba(240, 46, 170, 0.3) -10px 10px, rgba(240, 46, 170, 0.2) -15px 15px, rgba(240, 46, 170, 0.1) -20px 20px, rgba(240, 46, 170, 0.05) -25px 25px;
-
-
-animation: box2 1s infinite ease alternate ;
-@keyframes box2 {
-  to {
-    transform: translateY(70px);
-  }
-}
-
-@media only screen and (max-width: 780px) {
-  height: auto;
-  width: auto;
-  padding: 10px;
-}
-`
-
-
-
-const Box7 = styled.div`
-padding: 5px;
-margin: 10px;
-width:50%;
-height: 600px;
-border-style: solid;
-border-width: 5px;
-box-shadow: rgba(240, 46, 170, 0.4) -5px 5px, rgba(240, 46, 170, 0.3) -10px 10px, rgba(240, 46, 170, 0.2) -15px 15px, rgba(240, 46, 170, 0.1) -20px 20px, rgba(240, 46, 170, 0.05) -25px 25px;
-
-animation: box3 1s infinite ease alternate ;
-@keyframes box3 {
-  to {display: flex;
-    justify-content: space-between;
-    transform: translateY(70px);
-  }
-}
-@media only screen and (max-width: 780px) {
-  height: auto;
-  width: auto;
-  padding: 10px;
-}
-`
-const Box8 = styled.div`
-padding: 5px;
-margin: 10px;
-height: 650px;
-border-style: solid;
-border-width: 5px;
-width:50%;
-box-shadow: rgba(240, 46, 170, 0.4) -5px 5px, rgba(240, 46, 170, 0.3) -10px 10px, rgba(240, 46, 170, 0.2) -15px 15px, rgba(240, 46, 170, 0.1) -20px 20px, rgba(240, 46, 170, 0.05) -25px 25px;
-
-
-animation: box4 1s infinite ease alternate ;
-@keyframes box4 {
-  to {
-    transform: translateY(70px);
-  }
-}
-@media only screen and (max-width: 780px) {
-  height: auto;
-  width: auto;
-  padding: 10px;
- 
-}
-`
-
-const Titlex = styled.h1`
-font-size: 74px;
-
-
+gap: 10px;
+width: 60px;
+height; 50px;
 
 `;
 
@@ -408,65 +286,64 @@ const Carrosel = () => {
   return (
     <Section>
       <Container> 
-        <Titlex> Linguagens</Titlex>
     
         <ContainerBox>    
           <Box1>
-            <Title> Java </Title>
+            <Title>Java<Img src={IMGdois}/></Title>
             
-            <Img> </Img>
 
-            <List>
+        
             
-              <Ferramentas> Spring </Ferramentas>
+              <Ferramentas > Spring </Ferramentas>
               <Ferramentas> intelij </Ferramentas>
               <Ferramentas>  </Ferramentas>
               
               
-            </List>
+          
             <Button> Projects</Button>
           </Box1>
 
           <Box2>
-            <Title> Typescript </Title>
-            <Img> </Img>
-            <List>
+            <Title> Typescript <Img src={IMGtres}/> </Title>
+            
+        
               <Ferramentas> Vscode</Ferramentas>
               <Ferramentas> Nodsjs </Ferramentas>
               <Ferramentas> vercel </Ferramentas>
 
               
               
-            </List>
+          
             <Button> Projects</Button>
           </Box2>
 
 
           <Box3>
-            <Title> SQL</Title>
-            <Img> </Img>
-            <List>
+            <Title> SQL <Img src={IMGquatro}/></Title>
+        
               <Ferramentas> Vim </Ferramentas>
               <Ferramentas> . </Ferramentas>
               <Ferramentas>  . </Ferramentas>
-            </List>
+          
             <Button> Projects</Button>
           </Box3>
 
 
           <Box4>
-            <Title> Assembly and C </Title>
-            <Img> </Img>
-            <List>
+            <Title>Assembly<Img src={IMGcinco}/></Title>
+          
               <Ferramentas> Nasm </Ferramentas>
               <Ferramentas> gcc </Ferramentas>
-            </List>
+            
             <Button> Projects</Button>
           </Box4>
+         </ContainerBox>
 
-          
 
-         </ContainerBox> 
+         
+
+
+             
 
       
         </Container>
