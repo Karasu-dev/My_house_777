e55fedbfa02d4caea192c70c1a671d64af09bbce
aquillae7 <aquillae777@gmail.com>
2023-10-31 21:25:09 -0300

    test

---
+++ 7/Componentes/FooterMain.tsx
@@ -0,0 +1,16 @@
+import { Footer } from '@pmndrs/branding';
+
+export const  Overlay =()=> {
+  return (
+    <>
+      <Footer
+        date="28. January"
+        year="2021"
+        link1={<a href="https://github.com/pmndrs/drei">pmndrs/drei</a>}
+        link2={<a href="https://codesandbox.io/s/drei-reflector-bfplr">s/drei-reflector-bfplr</a>}
+      />
+    </>
+  )
+}
+
+export default Overlay;
