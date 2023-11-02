8a3ba939bc02a361581dde2e91d063f4b7009655
aquillae7 <aquillae777@gmail.com>
2023-11-02 17:40:41 -0300

    up

--- 7/Componentes/MainPage.tsx
+++ 7/Componentes/MainPage.tsx
@@ -33,8 +33,8 @@ function VideoText(props:any) {
   const [video] = useState(() => Object.assign(document.createElement('video'), { src: '/drei.mp4', crossOrigin: 'Anonymous', loop: true, muted: true }))
   useEffect(() => void video.play(), [video])
   return (
-    <Text font="/Inter-Bold.woff" fontSize={3} letterSpacing={-0.06} {...props}>
-      Aquilae
+    <Text font="/Inter-Bold.woff" fontSize={2} letterSpacing={-0.06} {...props}>
+      Karasu
       <meshBasicMaterial toneMapped={false}>
         <videoTexture attach="map" args={[video]} encoding={THREE.sRGBEncoding} />
       </meshBasicMaterial>
