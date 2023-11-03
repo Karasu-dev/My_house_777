4b3c33827a3ccf6ae0cf878e8fa02a3eab768481
aquillae7 <aquillae777@gmail.com>
2023-11-02 21:24:53 -0300

    up

---
+++ 7/Componentes/Group.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Reflector, useTexture} from '@react-three/drei';
+import { Vector2 } from 'three';
+
+function GroundMesh() {
+  const [floor, normal] = useTexture(['/SurfaceImperfections003_1K_var1.jpg', '/SurfaceImperfections003_1K_Normal.jpg']);
+
+  const customNormalScale: Vector2 = new Vector2(2, 2);
+
+  return (
+    <mesh>
+      <planeBufferGeometry args={[10, 10]} />
+      <meshStandardMaterial
+        color="#a0a0a0"
+        metalness={0.4}
+        roughnessMap={floor}
+        normalMap={normal}
+        normalScale={customNormalScale}
+      />
+    </mesh>
+  );
+}
+
+function Ground() {
+  return (
+    <Reflector
+      blur={[400, 100]}
+      resolution={512}
+      args={[10, 10]}
+      mirror={0.5}
+      mixBlur={6}
+      mixStrength={1.5}
+      rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+    >
+      <GroundMesh />
+    </Reflector>
+  );
+}
+
+export default Ground;
